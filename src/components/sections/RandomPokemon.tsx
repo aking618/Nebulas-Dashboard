20bb3c04b30b7149c0431af621ec47f28a05cc40
Ayren King <ashsquads@gmail.com>
2022-05-03 17:00:41 -0500

    Added height and weight to pokemon

--- src/components/sections/RandomPokemon.tsx
+++ src/components/sections/RandomPokemon.tsx
@@ -39,6 +39,16 @@ const RandomPokemon = () => {
     return valueList.join(" ");
   };
 
+  const convertDecimetersToFeetAndInches = (value: number) => {
+    var feet = value / 3.048;
+    var inches = Math.floor(feet * 12) % 12;
+    return `${feet.toFixed(2)}ft. ${inches}in.`;
+  };
+
+  const convertHectogramsToPounds = (value: number) => {
+    return (value / 4.53592).toFixed(2);
+  };
+
   return (
     <div style={{ padding: "1rem" }}>
       {loading ? (
@@ -46,44 +56,57 @@ const RandomPokemon = () => {
       ) : (
         pokemon && (
           <>
-            <Grid container direction="row">
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              spacing={1}
+              style={{
+                margin: "0 auto",
+              }}
+            >
               {/* Column 1 */}
-              <Grid container direction="column" alignItems="center">
-                <Grid item>
-                  <Typography
-                    align="center"
-                    variant="h4"
-                    style={textStyle}
-                    key={pokemon.name}
-                  >
-                    {pokemon.name.charAt(0).toUpperCase() +
-                      pokemon.name.slice(1)}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                </Grid>
-                <Grid item>
-                  <Typography align="center" variant="h6" style={textStyle}>
-                    Pokedex Number: {pokemon.id}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography align="center" variant="h6" style={textStyle}>
-                    Types:{" "}
-                    {pokemon.types.map((type) => (
-                      <img
-                        src={`https://serebii.net/pokedex-bw/type/${type.type.name}.gif`}
-                        alt={type.type.name}
-                        key={type.type.name}
-                        style={{
-                          marginRight: "0.5rem",
-                          ...textStyle,
-                        }}
-                      />
-                    ))}
-                  </Typography>
-                </Grid>
+              <Grid item>
+                <Typography
+                  align="center"
+                  variant="h4"
+                  style={textStyle}
+                  key={pokemon.name}
+                >
+                  {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+              </Grid>
+              <Grid item>
+                <Typography align="center" variant="h6" style={textStyle}>
+                  Pokedex Number: {pokemon.id}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography align="center" variant="h6" style={textStyle}>
+                  Types:{" "}
+                  {pokemon.types.map((type) => (
+                    <img
+                      src={`https://serebii.net/pokedex-bw/type/${type.type.name}.gif`}
+                      alt={type.type.name}
+                      key={type.type.name}
+                      style={{
+                        marginRight: "0.5rem",
+                        ...textStyle,
+                      }}
+                    />
+                  ))}
+                </Typography>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                alignItems="start"
+                justifyContent="center"
+                spacing={2}
+              >
                 <Grid item>
                   <Typography align="center" variant="h6" style={textStyle}>
                     Abilities:{" "}
@@ -105,7 +128,7 @@ const RandomPokemon = () => {
                   </Typography>
                 </Grid>
                 <Grid item>
-                  <Typography align="center" variant="h6" style={textStyle}>
+                  <Typography variant="h6" style={textStyle} align="center">
                     Stats:{" "}
                     {pokemon.stats.map((stat) => (
                       <Typography
@@ -122,6 +145,17 @@ const RandomPokemon = () => {
                     ))}
                   </Typography>
                 </Grid>
+              </Grid>
+
+              <Grid item>
+                <Typography align="center" variant="h6" style={textStyle}>
+                  Height: {convertDecimetersToFeetAndInches(pokemon.height)}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography align="center" variant="h6" style={textStyle}>
+                  Weight: {convertHectogramsToPounds(pokemon.weight)}lbs.
+                </Typography>
               </Grid>
             </Grid>
           </>
